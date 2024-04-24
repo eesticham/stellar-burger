ca4f931dca92b18503cb12a5b73d83d01f46f0c4
Maximiliank <48432756+EestiChameleon@users.noreply.github.com>
2024-04-24 17:12:16 +0300

    fix after review. second try

--- src/components/protected-route/ProtectedRoute.tsx
+++ src/components/protected-route/ProtectedRoute.tsx
@@ -12,29 +12,6 @@ export const ProtectedRoute = ({
   children,
   onlyUnAuth = false
 }: ProtectedRouteProps) => {
-  // const location = useLocation();
-  // const userData = useSelector((state) => state.userData);
-
-  // if (!userData.isAuthChecked) {
-  //   return <Preloader />;
-  // }
-
-  // console.log(`user:`, userData);
-  // console.log(`onlyUnAuth:`, onlyUnAuth);
-
-  // const isAuthenticated = userData.user.email != "" && userData.user.name != "";
-
-  // if (!onlyUnAuth && !isAuthenticated ) {
-  //   return <Navigate replace to='/login' state={{ from: location }} />;
-  // }
-
-  // if (onlyUnAuth && userData.user) {
-  //   const from = location.state?.from || { pathname: '/' };
-  //   return <Navigate replace to={from} />;
-  // }
-  // console.log(`onlyUnAuth[${onlyUnAuth}] && userData.user[${userData.user}] => ${onlyUnAuth && userData.user}`);
-
-  // return children;
   const isAuthChecked = useSelector((store) => store.userData.isAuthChecked);
   const user = useSelector((store) => store.userData.user);
   const location = useLocation();
